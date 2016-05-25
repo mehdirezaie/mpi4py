aebe14d4e2d3db2df5b9dd15cf5186b979bb0ea0
Mehdi <mehdirezaie1990@gmail.com>
2016-05-25 14:42:44 -0400

    add new pictures

--- bcast.py
+++ bcast.py
@@ -14,9 +14,11 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
-data = {'key1' : [10,10.1,10+11j],
-           'key2' : ( 'mpi4py', 'python'),
-           'key3':array([1,2,3])}
+data = {
+		'key1' : [10,10.1,10+11j],
+        'key2' : ('mpi4py' , 'python'),
+        'key3' : array([1, 2, 3])
+		}
 else:
    data = None
 
