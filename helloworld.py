d7f6f35fdd42147ac99faf16c320cc60cd2e2097
Mehdi Rezaie <medirz90@icloud.com>
2017-07-28 03:03:55 -0400

    helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 #	Example of MPI Hello
-#	last edit: April 23
-#	to run: mpirun -np 2 python ./hello_mpi.py
+#	last edit: Jul 28 2017
+#	to run: mpirun -np 2 python ./helloworld.py
 
 from mpi4py import MPI
 
@@ -11,9 +11,10 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 name = MPI.Get_processor_name()
 
+
 if rank ==0:
-   print "Hello World from rank %d from size %d running on %s ..." % (rank, size, name)
+   print("Hello World from rank {} from size {} running on {} ...".format(rank, size, name))
 else:
-   print "Goodbye World from rank %d from size %d running on %s ..." % (rank, size, name)
+   print("Goodbye World from rank {} from size {} running on {} ...".format(rank, size, name))
 
 comm.Barrier()
